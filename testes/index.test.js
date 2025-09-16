29113c5abaac05ddc8da94de4986dd89ab62996c
André Felipe Pellin <116200583+afpellin@users.noreply.github.com>
2025-09-15 23:14:33 -0300

    Update index.test.js

--- testes/index.test.js
+++ testes/index.test.js
@@ -1,31 +1,29 @@
-const request = require('supertest');
-const app = require('../index');
+const request = require("supertest");
+const app = require("../index"); // importa o app do index.js
 
-describe("Testes da aplicação Express", () => {
-  
-  test("Servidor responde com status 200 na rota /", async () => {
-    const res = await request(app).get('/');
+describe("Testes da aplicação Node.js", () => {
+  test("Deve retornar 200 na rota raiz /", async () => {
+    const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
   });
 
-  test("Resposta da rota / contém a mensagem correta", async () => {
-    const res = await request(app).get('/');
+  test("Deve retornar mensagem correta na rota raiz /", async () => {
+    const res = await request(app).get("/");
     expect(res.text).toContain("Servidor rodando com sucesso!");
   });
 
-  test("Array de tecnologias contém 'docker'", () => {
-    const tecnologias = ["node", "express", "docker", "jest"];
-    expect(tecnologias).toContain("docker");
+  test("Deve retornar 404 em rota inexistente", async () => {
+    const res = await request(app).get("/rota-invalida");
+    expect(res.statusCode).toBe(404);
   });
 
-  test("Objeto usuário deve conter nome e idade", () => {
-    const user = { nome: "André", idade: 38 };
-    expect(user).toHaveProperty("nome");
-    expect(user).toHaveProperty("idade");
+  test("Objeto deve conter chave nome", () => {
+    const usuario = { nome: "André", idade: 38 };
+    expect(usuario).toHaveProperty("nome");
   });
 
-  test("2 + 3 deve ser igual a 5", () => {
-    expect(2 + 3).toBe(5);
+  test("Soma deve funcionar corretamente", () => {
+    const soma = (a, b) => a + b;
+    expect(soma(2, 3)).toBe(5);
   });
-
 });
